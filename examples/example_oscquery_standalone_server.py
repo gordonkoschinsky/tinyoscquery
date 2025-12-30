a0c3af2979dadd73d33accf1be98dce908e1b86d
Frank <4243101+gordonkoschinsky@users.noreply.github.com>
2025-12-30 15:27:51 +0100

    Improve standalone server example

--- examples/example_oscquery_standalone_server.py
+++ examples/example_oscquery_standalone_server.py
@@ -6,53 +6,66 @@ from pythonoscquery.shared.osc_address_space import OSCAddressSpace
 from pythonoscquery.shared.osc_path_node import OSCPathNode
 
 logging.basicConfig()
-logger = logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
+    # Configure the osc address space and map each method node on the python-osc dispatcher
     osc_address_space = OSCAddressSpace()
+
     osc_address_space.add_node(
         OSCPathNode(
-            "/testing/is/cool",
-            value=99,
-            access=OSCAccess.READONLY_VALUE,
-            description="Read only int value",
-        )
-    )
-    osc_address_space.add_node(
-        OSCPathNode("/testing/is/good", value=False, access=OSCAccess.READWRITE_VALUE)
-    )
-    osc_address_space.add_node(
-        OSCPathNode(
-            "/testing/is/marvelous",
-            value=[False, 123, 567.8, "bar"],
+            "/test/writable/float",
+            value=99.0,
             access=OSCAccess.READWRITE_VALUE,
-        )
-    )
-    osc_address_space.add_node(
-        OSCPathNode(
-            "/testing/is/required",
-            value=[False, True],
-            access=OSCAccess.READWRITE_VALUE,
-        )
-    )
-    osc_address_space.add_node(
-        OSCPathNode(
-            "/testing/is/ok",
-            value=[1, 5],
-            access=OSCAccess.READWRITE_VALUE,
-        )
-    )
-    osc_address_space.add_node(
-        OSCPathNode(
-            "/write_only",
-            value=1,
-            access=OSCAccess.WRITEONLY_VALUE,
+            description="Read/write float value",
         )
     )
 
-    oscqs = OSCQueryService(osc_address_space, "Test-Service", 9020, 9020)
+    osc_address_space.add_node(
+        OSCPathNode(
+            "/test/writable/bool",
+            value=False,
+            access=OSCAccess.READWRITE_VALUE,
+            description="Read/write boolean value",
+        )
+    )
+
+    osc_address_space.add_node(
+        OSCPathNode(
+            "/test/writable/integer",
+            value=12,
+            access=OSCAccess.READWRITE_VALUE,
+            description="Read/write int value",
+        )
+    )
+
+    osc_address_space.add_node(
+        OSCPathNode(
+            "/test/writable/string",
+            value="Hello",
+            access=OSCAccess.READWRITE_VALUE,
+            description="Read/write string value",
+        )
+    )
+
+    osc_address_space.add_node(
+        OSCPathNode(
+            "/test/readable/string",
+            value="Hello",
+            access=OSCAccess.READONLY_VALUE,
+            description="Read-only string value",
+        )
+    )
+
+    osc_ip = "127.0.0.1"
+    oscquery_port = 9020
+    osc_port = 9021
+
+    oscqs = OSCQueryService(
+        osc_address_space, "Test-Service", oscquery_port, osc_port, osc_ip
+    )
 
     logger.debug("Server is up and serving address space %s", osc_address_space)
 
